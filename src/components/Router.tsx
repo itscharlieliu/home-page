a682ba86572589a3917f15b46e7b5c9b0dd76119
Charlie <itscharlieliu@gmail.com>
2020-11-22 18:34:04 -0800

    Add selected works screen

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 
-import { ABOUT } from "../defs/routerPaths";
+import { ABOUT, SELECTED_WORKS } from "../defs/routerPaths";
 
 import AboutScreen from "./screens/AboutScreen";
+import SelectedWorksScreen from "./screens/SelectedWorksScreen";
 
 const Router = (): JSX.Element => {
     return (
         <Switch>
             <Route path={ABOUT} exact component={AboutScreen} />
+            <Route path={SELECTED_WORKS} exact component={SelectedWorksScreen} />
             <Redirect to={ABOUT} />
         </Switch>
     );
---
+++ src/components/screens/SelectedWorksScreen.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const ScreenContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+`;
+
+const SelectedWorksScreen = (): JSX.Element => {
+    return <ScreenContainer>Test</ScreenContainer>;
+};
+
+export default SelectedWorksScreen;
