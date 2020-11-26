4f890c7e4b3508ee19e4bb19034a686321a46831
Charlie <itscharlieliu@gmail.com>
2020-11-25 17:47:11 -0800

    Add contact screen

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 
-import { ABOUT, SELECTED_WORKS } from "../defs/routerPaths";
+import { ABOUT, CONTACT, SELECTED_WORKS } from "../defs/routerPaths";
 
 import AboutScreen from "./screens/AboutScreen";
+import ContactScreen from "./screens/ContactScreen";
 import SelectedWorksScreen from "./screens/SelectedWorksScreen";
 
 const Router = (): JSX.Element => {
@@ -11,6 +12,7 @@ const Router = (): JSX.Element => {
         <Switch>
             <Route path={ABOUT} exact component={AboutScreen} />
             <Route path={SELECTED_WORKS} exact component={SelectedWorksScreen} />
+            <Route path={CONTACT} exact component={ContactScreen} />
             <Redirect to={ABOUT} />
         </Switch>
     );
---
+++ src/components/screens/ContactScreen.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContactScreen = (): JSX.Element => {
+    return <div>contact</div>
+}
+
+export default ContactScreen
